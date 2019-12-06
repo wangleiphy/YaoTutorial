0d1690702ae12d950b16bd731d23aab579a9691c
Lei Wang <wangleiphy@gmail.com>
2019-12-06 10:09:03 +0800

    forward mode

--- vqe.jl
+++ vqe.jl
@@ -1,7 +1,7 @@
 using Yao, Yao.AD, YaoExtensions
 
 n = 4
-d = 10
+d = 3
 circuit = dispatch!(variational_circuit(n, d),:random)
 
 gatecount(circuit)
@@ -9,7 +9,11 @@ gatecount(circuit)
 h = heisenberg(n)
 
 for i in 1:100
+      #reverse-mode
       _, grad = expect'(h, zero_state(n) => circuit)
+      #forward mode
+      #grad = my_faithful_grad(h, zero_state(n) => circuit; nshots=1)
+
       dispatch!(-, circuit, 1e-2 * grad)
       println("Step $i, energy = $(real.(expect(h, zero_state(n)=>circuit)))")
 end
