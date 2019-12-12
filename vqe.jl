7ce3229e0b90c464e524da4f60fa8fefc837eec8
Lei Wang <wangleiphy@gmail.com>
2019-12-12 17:43:54 +0800

    simplify

--- vqe.jl
+++ vqe.jl
@@ -1,6 +1,5 @@
 using Yao, Yao.AD, YaoExtensions
 
-include("faithful_grad.jl")
 n = 4
 d = 5
 circuit = dispatch!(variational_circuit(n, d),:random)
@@ -13,7 +12,7 @@ for i in 1:1000
       #reverse-mode
       # _, grad = expect'(h, zero_state(n) => circuit)
       #forward mode
-      grad = my_faithful_grad(h, zero_state(n) => circuit; nshots=100)
+      grad = faithful_grad(h, zero_state(n) => circuit; nshots=100)
 
       dispatch!(-, circuit, 1e-2 * grad)
       println("Step $i, energy = $(real.(expect(h, zero_state(n)=>circuit)))")
